168265b8ee0ecf755187f14c4b268be6938aedd6
Dekam <ildararslavov10@gmail.com>
2023-09-09 11:24:35 +0500

    правка

--- js/index.js
+++
@@ -1,9 +0,0 @@
-function setElementHeight() {
-    var windowHeight = window.innerHeight;
-    var element = document.querySelector('.page__main-block');
-    element.style.minHeight = windowHeight + 'px';
-}
-
-// Вызываем функцию при загрузке страницы и при изменении размеров окна
-window.addEventListener('resize', setElementHeight);
-window.addEventListener('load', setElementHeight);
